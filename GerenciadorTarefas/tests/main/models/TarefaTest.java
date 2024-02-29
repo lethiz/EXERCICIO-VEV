5a0bbe0cc8819c7bd24b7bf41913d3abca467654
Leticia <leticiaramosrodrigues23@gmail.com>
2024-02-29 11:01:19 -0300

    Início do desenvolvimento TDD do Gerenciador de Tarefas

---
+++ GerenciadorTarefas/tests/main/models/TarefaTest.java
@@ -0,0 +1,13 @@
+package main.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TarefaTest {
+
+    @BeforeEach
+    public void prepareTest(){
+
+    }
+
+}
