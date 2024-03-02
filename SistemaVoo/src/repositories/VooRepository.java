0e55a482043970747db8335ddefdd7dd9ae6f808
andre <andre.alves@ccc.ufcg.edu.br>
2024-03-02 10:02:08 -0300

    Adicionando criação da classe de Voo Repository

--- SistemaVoo/src/repositories/VooRepository.java
+++ SistemaVoo/src/repositories/VooRepository.java
@@ -1,4 +1,14 @@
 package repositories;
 
+import models.Voo;
+
+import java.util.ArrayList;
+
 public class VooRepository {
+
+    private ArrayList<Voo> voos;
+
+    public VooRepository(ArrayList<Voo> voos) {
+        this.voos = voos;
+    }
 }
--- SistemaVoo/tests/repositories/tests/VooRepositoryTest.java
+++ SistemaVoo/tests/repositories/tests/VooRepositoryTest.java
@@ -1,15 +1,26 @@
 package repositories.tests;
 
+import models.Voo;
 import org.junit.Before;
+import org.junit.Test;
 import repositories.VooRepository;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class VooRepositoryTest {
 
     private static VooRepository vooRepository;
 
     @Before
-    private void setupVooRepository() {
-        vooRepository = new VooRepository();
+    public void setupVooRepository() {
+        ArrayList<Voo> voos = new ArrayList<Voo>();
+        vooRepository = new VooRepository(voos);
+    }
+
+    @Test
+    public void checkCreated() {
         assertNotNull(vooRepository);
     }
 }
