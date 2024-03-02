fe4ba6ab2c07f87adcb185ef1275f4cf529931cb
leticia <leticia.rodrigues@ccc.ufcg.edu.br>
2024-03-02 00:50:24 -0300

    Desenvolvimento do model de tarefas

--- GerenciadorTarefas/src/main/models/Tarefa.java
+++ GerenciadorTarefas/src/main/models/Tarefa.java
@@ -9,36 +9,43 @@ public class Tarefa {
     private String dataVencimento;
     private TarefaPrioridade prioridade;
 
+    public Tarefa(String titulo, String descricao, String dataVencimento, TarefaPrioridade prioridade) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataVencimento = dataVencimento;
+        this.prioridade = prioridade;
+    }
+
 
     public String getTitulo() {
         return this.titulo;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setTitulo(String tituloNovo) {
+        this.titulo = tituloNovo;
     }
 
     public String getDescricao() {
         return this.descricao;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public void setDescricao(String descricaoNova) {
+        this.descricao = descricaoNova;
     }
 
     public String getDataVencimento() {
         return this.dataVencimento;
     }
 
-    public void setDataVencimento(String dataVencimento) {
-        this.dataVencimento = dataVencimento;
+    public void setDataVencimento(String dataVencimentoNova) {
+        this.dataVencimento = dataVencimentoNova;
     }
 
     public TarefaPrioridade getPrioridade() {
         return this.prioridade;
     }
 
-    public void setPrioridade(TarefaPrioridade prioridade) {
-        this.prioridade = prioridade;
+    public void setPrioridade(TarefaPrioridade prioridadeNova) {
+        this.prioridade = prioridadeNova;
     }
 }
