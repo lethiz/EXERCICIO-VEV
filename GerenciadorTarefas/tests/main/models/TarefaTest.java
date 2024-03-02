5fc05038078091f14ade7103c41075bf7696202f
leticia <leticia.rodrigues@ccc.ufcg.edu.br>
2024-03-02 00:28:30 -0300

    Adição de testes referentes a atualização do model de tarefa

--- GerenciadorTarefas/tests/main/models/TarefaTest.java
+++ GerenciadorTarefas/tests/main/models/TarefaTest.java
@@ -22,4 +22,16 @@ class TarefaTest {
         assertEquals("25/09/2024", tarefaCriada.getDataVencimento());
         assertEquals("PRIORIDADE-BAIXA", tarefaCriada.getPrioridade());
     }
+
+    @Test
+    public void criarTarefa(){
+        this.tarefaTeste.setTitulo("Título Atualizado");
+        this.tarefaTeste.setDescricao("Descrição Atualizada");
+        this.tarefaTeste.setDataVencimento("26/09/2024");
+        this.tarefaTeste.setPrioridade("PRIORIDADE-MÉDIA");
+        assertEquals("Título Atualizado", tarefaTeste.getTitulo());
+        assertEquals("Descrição Atualizado", tarefaTeste.getDescricao());
+        assertEquals("26/09/2024", tarefaTeste.getDataVencimento());
+        assertEquals("PRIORIDADE-MÉDIA", tarefaTeste.getPrioridade());
+    }
 }
