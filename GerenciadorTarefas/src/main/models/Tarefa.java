40d4511ff55b4554e9ec1f0f7c8bdc284f544db6
leticia <leticia.rodrigues@ccc.ufcg.edu.br>
2024-03-02 00:42:00 -0300

    Adição de atributos ao model de tarefa

--- GerenciadorTarefas/src/main/models/Tarefa.java
+++ GerenciadorTarefas/src/main/models/Tarefa.java
@@ -1,4 +1,44 @@
 package main.models;
 
+import main.util.TarefaPrioridade;
+
 public class Tarefa {
+
+    private String titulo;
+    private String descricao;
+    private String dataVencimento;
+    private TarefaPrioridade prioridade;
+
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDataVencimento() {
+        return this.dataVencimento;
+    }
+
+    public void setDataVencimento(String dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public TarefaPrioridade getPrioridade() {
+        return this.prioridade;
+    }
+
+    public void setPrioridade(TarefaPrioridade prioridade) {
+        this.prioridade = prioridade;
+    }
 }
