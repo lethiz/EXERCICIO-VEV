47a65f87b2493c0aba7b204cd67dd84c0ad19bb9
leticia <leticia.rodrigues@ccc.ufcg.edu.br>
2024-03-02 00:26:44 -0300

    Adição de testes referentes a criação do model de tarefa

--- GerenciadorTarefas/tests/main/models/TarefaTest.java
+++ GerenciadorTarefas/tests/main/models/TarefaTest.java
@@ -1,13 +1,25 @@
 package main.models;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TarefaTest {
 
-    @BeforeEach
-    public void prepareTest(){
+    Tarefa tarefaTeste;
 
+    @BeforeEach
+    public void prepararTest(){
+        this.tarefaTeste = new Tarefa("Título Testagen", "Descrição Testagem", "23/09/2024", "PRIORIDADE-ALTA");
     }
 
+    @Test
+    public void criarTarefa(){
+        this.tarefaCriada =  new Tarefa("Título Criado", "Descrição Criação", "25/09/2024", "PRIORIDADE-BAIXA");
+        assertEquals("Título Criado", tarefaCriada.getTitulo());
+        assertEquals("Descrição Criação", tarefaCriada.getDescricao());
+        assertEquals("25/09/2024", tarefaCriada.getDataVencimento());
+        assertEquals("PRIORIDADE-BAIXA", tarefaCriada.getPrioridade());
+    }
 }
