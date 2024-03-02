62a6d35b37b5ce608da85c3f314054e868d4ccc0
andre <andre.alves@ccc.ufcg.edu.br>
2024-03-02 19:05:26 -0300

    Adicionando teste para a classe reserva

--- SistemaVoo/tests/models/tests/ReservaTest.java
+++ SistemaVoo/tests/models/tests/ReservaTest.java
@@ -1,5 +1,6 @@
 package models.tests;
 
+import models.Reserva;
 import models.Voo;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,4 +10,10 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 public class ReservaTest {
 
+
+    @Test
+    public void checkCreateReserva() {
+        Reserva reserva = new Reserva();
+        assertNotNull(reserva);
+    }
 }
