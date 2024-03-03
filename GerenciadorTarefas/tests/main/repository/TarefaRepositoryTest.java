ee64feb997038d7ad7af10d9bb14127ebd5bbae6
leticia <leticia.rodrigues@ccc.ufcg.edu.br>
2024-03-03 00:04:06 -0300

    Inclusão de novos testes considerando exceções

--- GerenciadorTarefas/tests/main/repository/TarefaRepositoryTest.java
+++ GerenciadorTarefas/tests/main/repository/TarefaRepositoryTest.java
@@ -1,9 +1,11 @@
 package main.repository;
 
+import main.exceptions.InvalidIDException;
 import main.exceptions.Model.InvalidDataVencimentoException;
 import main.exceptions.Model.InvalidDescricaoException;
 import main.exceptions.Model.InvalidPrioridadeException;
 import main.exceptions.Model.InvalidTituloException;
+import main.exceptions.Repository.InvalidTarefaException;
 import main.models.Tarefa;
 import main.util.TarefaPrioridade;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,7 +23,7 @@ class TarefaRepositoryTest {
     private ITarefaRepository tarefaRepository;
 
     @BeforeEach
-    public void prepararTest() throws InvalidPrioridadeException, InvalidDataVencimentoException, InvalidDescricaoException, InvalidTituloException {
+    public void prepararTest() throws InvalidPrioridadeException, InvalidDataVencimentoException, InvalidDescricaoException, InvalidTituloException, InvalidTarefaException {
         this.tarefaRepository = new TarefaRepository();
         this.tarefaTeste = new Tarefa("Título Testagem", "Descrição Testagem", "23/09/2024", TarefaPrioridade.PRIORIDADE_ALTA);
         this.tarefaTesteOutra = new Tarefa("Título Outra Testagem", "Descrição Outra Testagem", "25/09/2024", TarefaPrioridade.PRIORIDADE_MEDIA);
@@ -37,7 +39,7 @@ class TarefaRepositoryTest {
     }
 
     @Test
-    void recuperarTarefaTeste() {
+    void recuperarTarefaTeste() throws InvalidTarefaException, InvalidIDException {
         Tarefa tarefaRecuperada =  this.tarefaRepository.recuperarTarefa(tarefaTeste.getId());
         assertEquals("Título Testagem", tarefaRecuperada.getTitulo());
         assertEquals("Descrição Testagem", tarefaRecuperada.getDescricao());
@@ -47,7 +49,14 @@ class TarefaRepositoryTest {
     }
 
     @Test
-    void buscarTarefasTeste() {
+    void recuperarTarefaNulaTeste() {
+        assertThrows(InvalidIDException.class, () -> {
+            this.tarefaRepository.recuperarTarefa(null);
+        });
+    }
+
+    @Test
+    void buscarTarefasTeste() throws InvalidTituloException {
         List<Tarefa> tarefasBuscadas =  this.tarefaRepository.buscarTarefas("Título Testagem");
         assertEquals(1, tarefasBuscadas.size());
         assertEquals(tarefaTeste, tarefasBuscadas.getFirst());
@@ -55,7 +64,7 @@ class TarefaRepositoryTest {
     }
 
     @Test
-    void adicionarTarefaTeste() throws InvalidPrioridadeException, InvalidDataVencimentoException, InvalidDescricaoException, InvalidTituloException {
+    void adicionarTarefaTeste() throws InvalidPrioridadeException, InvalidDataVencimentoException, InvalidDescricaoException, InvalidTituloException, InvalidTarefaException {
         Tarefa tarefaCriada = new Tarefa("Título Criada", "Descrição Criada", "26/09/2024", TarefaPrioridade.PRIORIDADE_BAIXA);
         Boolean tarefa =  this.tarefaRepository.adicionarTarefa(tarefaCriada);
         assertTrue(tarefa);
@@ -66,7 +75,14 @@ class TarefaRepositoryTest {
     }
 
     @Test
-    void atualizarTarefaTeste() throws InvalidDescricaoException {
+    void adicionarTarefaNulaTeste() {
+        assertThrows(InvalidTarefaException.class, () -> {
+            this.tarefaRepository.adicionarTarefa(null);
+        });
+    }
+
+    @Test
+    void atualizarTarefaTeste() throws InvalidDescricaoException, InvalidTarefaException, InvalidPrioridadeException, InvalidDataVencimentoException, InvalidTituloException, InvalidIDException {
         this.tarefaTeste.setDescricao("Nova Descrição");
         Boolean tarefa = this.tarefaRepository.atualizarTarefa(tarefaTeste);
         assertTrue(tarefa);
@@ -75,13 +91,22 @@ class TarefaRepositoryTest {
         Integer contagem = tarefas.size();
         assertEquals(contagem, 2);
 
-        Tarefa tarefaRecuperada =  this.tarefaRepository.recuperarTarefa(tarefaTeste.getId());
+        Tarefa tarefaRecuperada = null;
+        tarefaRecuperada = this.tarefaRepository.recuperarTarefa(tarefaTeste.getId());
+
         assertEquals("Nova Descrição", tarefaRecuperada.getDescricao());
 
     }
 
     @Test
-    void removerTarefaTeste() {
+    void atualizarTarefaNulaTeste(){
+        assertThrows(InvalidTarefaException.class, () -> {
+            this.tarefaRepository.atualizarTarefa(null);
+        });
+    }
+
+    @Test
+    void removerTarefaTeste() throws InvalidIDException, InvalidTarefaException {
         Boolean tarefaRemovida =  this.tarefaRepository.removerTarefa(tarefaTeste.getId());
         assertTrue(tarefaRemovida);
 
@@ -89,5 +114,12 @@ class TarefaRepositoryTest {
         Integer contagem = tarefas.size();
         assertEquals(contagem, 1);
 
+    }
+
+    @Test
+    void removerTarefaNulaTeste() {
+        assertThrows(InvalidTarefaException.class, () -> {
+                this.tarefaRepository.removerTarefa(null);
+            });
     }
 }
