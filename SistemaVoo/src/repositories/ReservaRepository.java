f4c570f30745270785c248908ec82cce403b8a0e
andre <andre.alves@ccc.ufcg.edu.br>
2024-03-03 17:37:47 -0300

    Adicionando testes para a funcionalidade de tamanho do repositorio de reserva

--- SistemaVoo/src/repositories/ReservaRepository.java
+++ SistemaVoo/src/repositories/ReservaRepository.java
@@ -1,11 +1,22 @@
 package repositories;
 
 import models.Reserva;
+import models.Voo;
 
 import java.util.ArrayList;
 
 public class ReservaRepository {
 
-    ArrayList<Reserva> reservas = new ArrayList<Reserva>();
+    private ArrayList<Reserva> reservas;
+
+    private VooRepository vooRepository;
+
+    public ReservaRepository() {
+        this.reservas = new ArrayList<Reserva>();
+        this.vooRepository = new VooRepository();
+    }
+
+
+
 
 }
--- SistemaVoo/tests/repositories/tests/ReservaRepositoryTest.java
+++ SistemaVoo/tests/repositories/tests/ReservaRepositoryTest.java
@@ -1,17 +1,37 @@
 package repositories.tests;
 
+import models.Reserva;
+import models.Voo;
 import org.junit.Test;
 import repositories.ReservaRepository;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ReservaRepositoryTest {
 
+    private Voo voo = new Voo("Brasil", "EUA", 100, LocalDate.now(), 2302);
+    private UUID vooId = voo.getID();
+    public ReservaRepository reservaRepository = new ReservaRepository();
+
     @Test
     public void ReservaRepositoryTest() {
-        ReservaRepository reservaRepository = new ReservaRepository();
-        assertNotNull(reservaRepository);
+        ReservaRepository reservaRepositoryConstructor = new ReservaRepository();
+        assertNotNull(reservaRepositoryConstructor);
     }
 
+    @Test
+    public void checkReservaRepositorySize() {
+        assertEquals(0, reservaRepository.getTotalReservas());
+    }
 
+    /*@Test
+    public void checkCriarReserva() {
+        Reserva reserva = new Reserva("Clay", "83999991334", vooId, 3, 2302 * 3);
+        reservaRepository.criarReserva(reserva);
+
+    }*/
 }
