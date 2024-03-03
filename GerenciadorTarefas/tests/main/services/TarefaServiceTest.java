c18f01ae0e239764dac425b6ed4a65b0093ee828
leticia <leticia.rodrigues@ccc.ufcg.edu.br>
2024-03-03 16:46:31 -0300

    Testes de buscar tarefas do service de tarefas

--- GerenciadorTarefas/tests/main/services/TarefaServiceTest.java
+++ GerenciadorTarefas/tests/main/services/TarefaServiceTest.java
@@ -1,9 +1,15 @@
 package main.services;
 
 import main.exceptions.Model.*;
+import main.exceptions.Service.InvalidComparacaoException;
 import main.models.Tarefa;
 import main.util.TarefaPrioridade;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TarefaServiceTest {
     private Tarefa tarefaTeste;
@@ -17,6 +23,60 @@ public class TarefaServiceTest {
         this.tarefaTeste = this.tarefaService.criarTarefa("Título Testagem", "Descrição Testagem", "23/09/2024", TarefaPrioridade.PRIORIDADE_ALTA);
         this.tarefaTesteOutra =  this.tarefaService.criarTarefa("Título Outra Testagem", "Descrição Outra Testagem", "25/09/2024", TarefaPrioridade.PRIORIDADE_MEDIA);
         this.tarefaTesteFinal = this.tarefaService.criarTarefa("Título Testagem Final", "Descrição Testagem Final", "24/09/2024", TarefaPrioridade.PRIORIDADE_ALTA);
+    }
+
+    @Test
+    void buscarTarefasTeste() {
+        List<Tarefa> tarefasBuscadas = this.tarefaService.buscarTarefas();
+        assertEquals(3, tarefasBuscadas.size());
+
+        Tarefa[] tarefasTarget = {tarefaTeste, tarefaTesteOutra, tarefaTesteFinal};
+        assertArrayEquals(tarefasBuscadas.toArray(), tarefasTarget);
+    }
+
+    @Test
+    void buscarTarefasOrdenadasTeste() throws InvalidComparacaoException {
+        List<Tarefa> tarefasBuscadas = this.tarefaService.buscarTarefasOrdenadas("PRIORIDADE");
+        assertEquals(3, tarefasBuscadas.size());
+
+        Tarefa[] tarefasTarget = {tarefaTeste, tarefaTesteOutra, tarefaTesteFinal};
+        assertArrayEquals(tarefasBuscadas.toArray(), tarefasTarget);
+
+        assertEquals(tarefasBuscadas.get(0), tarefaTeste);
+        assertEquals(tarefasBuscadas.get(1), tarefaTesteOutra);
+        assertEquals(tarefasBuscadas.get(2), tarefaTesteFinal);
+    }
+
+    @Test
+    void buscarTarefasOrdenadasTesteAlternativo() throws InvalidComparacaoException {
+        List<Tarefa> tarefasBuscadas = this.tarefaService.buscarTarefasOrdenadas("DATA_VENCIMENTO");
+        assertEquals(3, tarefasBuscadas.size());
+
+        Tarefa[] tarefasTarget = {tarefaTeste, tarefaTesteOutra, tarefaTesteFinal};
+        assertArrayEquals(tarefasBuscadas.toArray(), tarefasTarget);
+
+        assertEquals(tarefasBuscadas.get(0), tarefaTeste);
+        assertEquals(tarefasBuscadas.get(1), tarefaTesteFinal);
+        assertEquals(tarefasBuscadas.get(2), tarefaTesteOutra);
+    }
+    @Test
+    void buscarTarefasOrdenadasTesteFinal() throws InvalidComparacaoException {
+        List<Tarefa> tarefasBuscadas = this.tarefaService.buscarTarefasOrdenadas("OUTRA_COISA");
+        assertEquals(3, tarefasBuscadas.size());
+
+        Tarefa[] tarefasTarget = {tarefaTeste, tarefaTesteOutra, tarefaTesteFinal};
+        assertArrayEquals(tarefasBuscadas.toArray(), tarefasTarget);
+
+        assertEquals(tarefasBuscadas.get(0),tarefaTesteOutra);
+        assertEquals(tarefasBuscadas.get(1), tarefaTeste);
+        assertEquals(tarefasBuscadas.get(2), tarefaTesteOutra);
+    }
+
+    @Test
+    void buscarTarefasOrdenadasInvalidasTeste()  {
+        assertThrows(InvalidComparacaoException.class, () -> {
+            this.tarefaService.buscarTarefasOrdenadas(null);
+        });
     }
 
 }
