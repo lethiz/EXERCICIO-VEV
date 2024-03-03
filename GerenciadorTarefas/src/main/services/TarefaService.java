01edf7b8977808e0078944864b648b79b2f05e03
leticia <leticia.rodrigues@ccc.ufcg.edu.br>
2024-03-03 16:54:56 -0300

    Adição de método de buscar tarefas com ordem do service de tarefas

--- GerenciadorTarefas/src/main/services/TarefaService.java
+++ GerenciadorTarefas/src/main/services/TarefaService.java
@@ -5,29 +5,60 @@ import main.exceptions.Model.*;
 import main.exceptions.Service.InvalidComparacaoException;
 import main.exceptions.Service.InvalidTarefaException;
 import main.models.Tarefa;
+import main.repository.ITarefaRepository;
+import main.util.CheckInvalids;
+import main.util.DataVencimentoComparator;
 import main.util.TarefaPrioridade;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class TarefaService implements ITarefaService{
-    @Override
-    public List<Tarefa> buscarTarefas() {
-        return null;
+
+    private ITarefaRepository tarefasRepository;
+
+    private final CheckInvalids checkInvalids = new CheckInvalids();
+
+    public List<Tarefa> buscarTarefas()  {
+        return tarefasRepository.recuperarTarefas().values().stream().toList();
     }
 
-    @Override
-    public List<Tarefa> buscarTarefasOrdenadas(String primeiraComparacao) throws InvalidComparacaoException {
-        return null;
+    public  List<Tarefa>  buscarTarefasOrdenadas(String primeiraComparacao) throws InvalidComparacaoException {
+        List<Tarefa> tarefasRecuperadas =  this.buscarTarefas();
+
+        if(primeiraComparacao == null){
+            throw new InvalidComparacaoException("o parâmetro de comparação não pode ser nulo");
+        }
+
+        String comparativoInicial = primeiraComparacao.toUpperCase();
+
+
+        if(Objects.equals(comparativoInicial, "PRIORIDADE")) return tarefasRecuperadas.stream().sorted(
+                Comparator.comparing(Tarefa::getPrioridade)
+                        .thenComparing(new DataVencimentoComparator())
+        ).collect(Collectors.toList());
+        else if(Objects.equals(comparativoInicial, "DATA_VENCIMENTO")) return tarefasRecuperadas.stream().sorted(
+                Comparator.comparing(Tarefa::getPrioridade)
+                        .thenComparing(new DataVencimentoComparator())
+        ).collect(Collectors.toList());
+        else return tarefasRecuperadas.stream().sorted(
+                    Comparator.comparing(Tarefa::getPrioridade)
+                            .thenComparing((Tarefa::getTitulo))
+            ).collect(Collectors.toList());
+
+    }
+    public  List<Tarefa> buscarTarefasPrioridade()  {
+        List<Tarefa> tarefaRecuperadas = this.buscarTarefas();
+
+        return tarefaRecuperadas.stream().sorted(new DataVencimentoComparator()).toList();
     }
 
-    @Override
-    public List<Tarefa> buscarTarefasPrioridade() {
-        return null;
-    }
+    public  List<Tarefa> buscarTarefasDataVencimento()  {
+        List<Tarefa> tarefaRecuperadas = this.buscarTarefas();
 
-    @Override
-    public List<Tarefa> buscarTarefasDataVencimento() {
-        return null;
+        return tarefaRecuperadas.stream().sorted(Comparator.comparing(Tarefa::getPrioridade)).toList();
     }
 
     @Override
